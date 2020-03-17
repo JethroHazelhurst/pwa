4a28c59faeb295159fabc895cf80724fb3939898
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-03-17 14:46:22 +0000

    Add auth:api middleware to resource routes

--- routes/api.php
+++ routes/api.php
@@ -3,27 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/**
- *
- */
 Route::post('login', 'UserController@login');
-
-/**
- *
- */
 Route::post('register', 'UserController@register');
 
 /**
- *
+ * Resource routes are protected from unauthenticated users
  */
-Route::resource('/task', 'TaskController');
-
-/**
- *
- */
-Route::get('/category/{category}/tasks', 'CategoryController@tasks');
-
-/**
- *
- */
-Route::resource('/category', 'CategoryController');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::resource('/task', 'TaskController');
+    Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+    Route::resource('/category', 'CategoryController');
+});

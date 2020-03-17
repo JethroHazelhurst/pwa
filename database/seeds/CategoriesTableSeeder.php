b3bbf4f41d23f82d2b326480999ae2d05293441e
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-03-17 14:07:33 +0000

    Add model factories for categories and users

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Category;
 use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder
@@ -11,6 +12,16 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            'Ideas',
+            'On Going',
+            'Completed'
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
     }
 }

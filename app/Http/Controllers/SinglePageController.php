a5ca610f19598391d23735d1a158d607fb79bbff
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-03-17 16:26:51 +0000

    Add landing page controller method

--- app/Http/Controllers/SinglePageController.php
+++ app/Http/Controllers/SinglePageController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class SinglePageController extends Controller
 {
-    //
+    /**
+     * 
+     */
+    public function index()
+    {
+        return view("landing");
+    }
 }
